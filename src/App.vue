6e61c6c2719993c63c6a9f76e6637ba0cba96587
Jan Langheimer <langheimer@gmail.com>
2021-04-30 08:45:28 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>Username: {{username}}</div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+// @ts-ignore
+import ScatterJS from "@scatterjs/core";
+// @ts-ignore
+import ScatterEOS from "@scatterjs/eosjs2";
+
+const username = ref<string>("");
+
+onMounted(async () => {
+  ScatterJS.plugins(new ScatterEOS());
+
+  const network = ScatterJS.Network.fromJson({
+    blockchain: "eos",
+    chainId: "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906",
+    host: "nodes.get-scatter.com",
+    port: 443,
+    protocol: "https",
+  });
+
+  const connected = await ScatterJS.connect("Test App", { network });
+  if (!connected) return console.error("no scatter");
+
+  const id = await ScatterJS.login();
+  if (!id) return console.error("no identity");
+
+  const account = ScatterJS.account("eos");
+
+  if (!account) return console.error("no account");
+  console.log(account)
+  username.value = account.name
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
